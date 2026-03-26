b5387dc8892ed56100e8f7026c9bebca7697f048
Yash Shinde <yash73shinde@gmail.com>
2026-03-26 13:15:02 +0530

    add infrastructure automation code

---
+++ infrastructure/terraform/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "ap-south-1"
+}
+
+# EC2 INSTANCE
+resource "aws_instance" "web_server" {
+
+  ami           = "ami-019715e0d74f695be"   # Ubuntu AMI in ap-south-1
+  instance_type = "t2.medium"
+
+  key_name = "argocd-key"  # name of keypair in AWS, not .pem file
+
+  vpc_security_group_ids = ["sg-0d23326df3bbf4798"]
+
+  root_block_device {
+    volume_size = 24
+    volume_type = "gp3"
+  } 
+
+  tags = {
+    Name = "terraform-ec2"
+  }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.web_server.public_ip
+}
+
+output "ec2_private_ip" {
+  value = aws_instance.web_server.private_ip
+}
